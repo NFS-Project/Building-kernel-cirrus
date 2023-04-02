7dffa270689b9ff6244cb9112fbdfc16dc54a4d5
FinixDev™ <jarbull87@gmail.com>
2023-04-02 23:57:02 +0700

    Test

--- download.sh
+++ download.sh
@@ -2,6 +2,3 @@
 
 echo "Downloading Kernel Sources.."
 git clone --depth 1 $KERNEL_SOURCE -b $KERNEL_BRANCH $CIRRUS_WORKING_DIR/$DEVICE_CODENAME
-echo ""
-echo "Downloading Toolchain.."
-git clone --depth 1 $TOOLCHAIN_SOURCE -b $TOOLCHAIN_BRANCH $CIRRUS_WORKING_DIR/CLANG
